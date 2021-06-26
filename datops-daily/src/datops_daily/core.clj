e17d68d2bea5ced2f5f0396068808944ec884ade
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-06-26 17:52:03 +0800

    Successfully calculate & update yeild

---
+++ datops-daily/src/datops_daily/core.clj
@@ -0,0 +1,13 @@
+(ns datops-daily.core
+  (:gen-class)
+  (:require
+   [taoensso.timbre :as timbre :refer [info warn error]]
+   [datops-daily.db-update :as dbu]))
+
+(defn -main
+  [& args]
+  (let [set-args (into #{} args)]
+    (when (set-args "--yields")
+      (info "Updating yields across active options.")
+      (dbu/update-live-options 1000)
+      (info "Done."))))
---
+++ datops-daily/src/datops_daily/db_update.clj
@@ -0,0 +1,47 @@
+(ns datops-daily.db-update
+  (:require
+   [clojure.java.jdbc :as db]
+   [clojure.data.json :as json]
+   [next.jdbc :as jdbc]
+   [next.jdbc.prepare :as jdbp]
+   [next.jdbc.types :as jdbt]))
+
+(def month-unit (* 3600 24 30))
+
+(defn calculate-monthly-yield
+  "Calculate the yield (premium/strike) and monthly yield of a given option"
+  [{:keys [contractsymbol strike ask bid lastprice expiration]}]
+  (let [yield (/ (or (when (> ask 0) ask) lastprice) strike)
+        now (/ (System/currentTimeMillis) 1000)
+        nb-months (/ (- expiration now) month-unit)]
+    [(float yield) (float (/ yield nb-months)) contractsymbol]))
+
+;; (calculate-monthly-yield {:strike 100 :ask 3 :expiration (+ (* 3 month-unit) (/ (System/currentTimeMillis) 1000))})
+;; => [0.03 0.01 nil]
+
+(def db (-> "resources/db.edn" slurp read-string))
+
+(defn get-live-options
+  "Allow to get options with pagination"
+  [limit & {:keys [last-seen]}]
+  (db/query
+   db
+   (str "SELECT * FROM live WHERE expiration > " (int (/ (System/currentTimeMillis) 1000))
+        (when last-seen
+          (str " AND contractsymbol > \"" last-seen "\""))
+        " ORDER BY contractsymbol LIMIT " limit)))
+
+(defn update-live-options
+  [limit & {:keys [last-seen]}]
+  (println "Last seen:" last-seen)
+  (let [options (get-live-options limit :last-seen last-seen)
+        yields (map calculate-monthly-yield options)
+        last-seen (-> options last :contractsymbol)]
+    (with-open [con (jdbc/get-connection db)
+                ps
+                (jdbc/prepare
+                 con
+                 ["UPDATE live SET yield=?, monthlyyield=? WHERE contractsymbol=?"])]
+      (jdbp/execute-batch! ps yields))
+    (when (not-empty yields)
+      (recur limit {:last-seen last-seen}))))

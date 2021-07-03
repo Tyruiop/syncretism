d23d7db8fc05758d3370d96f9c6b9f93080dba64
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-07-03 16:37:00 +0800

    Attempt to make test pass regardless of machine timezone

--- syncretism/test/syncretism/time_test.clj
+++ syncretism/test/syncretism/time_test.clj
@@ -3,15 +3,32 @@
    [clojure.test :refer :all]
    [syncretism.time :refer :all])
   (:import
-   [java.time ZoneId]))
+   [java.time ZoneId]
+   [java.sql Timestamp]))
+
+(def offset
+  (-
+   (-> 
+    (Timestamp. 0)
+    (.toLocalDateTime)
+    (.atZone (ZoneId/systemDefault))
+    java-time/as-map
+    :offset-seconds)
+
+   (-> 
+    (Timestamp. 0)
+    (.toLocalDateTime)
+    (.atZone (ZoneId/of "Europe/Prague"))
+    java-time/as-map
+    :offset-seconds)))
 
 (deftest test-market-time
   (testing "market-time should report correct market period"
     (let [tz (ZoneId/of "Europe/Prague")
-          ts-closed 1625103790000
-          ts-pre 1625118190000
-          ts-market 1625136190000
-          ts-post 1625161390000]
+          ts-closed (+ 1625103790000 offset)
+          ts-pre (+ 1625118190000 offset)
+          ts-market (+ 1625136190000 offset)
+          ts-post (+ 1625161390000 offset)]
       (is (= (market-time tz ts-closed) "CLOSED"))
       (is (= (market-time tz ts-pre) "PRE"))
       (is (= (market-time tz ts-market) "OPEN"))

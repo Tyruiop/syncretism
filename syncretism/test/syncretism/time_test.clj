2fcccb02eb7f3e81f8ade0480e7d1ebb0f9b97c2
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-07-03 17:09:21 +0800

    Hopefully test becomes timezone independent

--- syncretism/test/syncretism/time_test.clj
+++ syncretism/test/syncretism/time_test.clj
@@ -1,34 +1,36 @@
 (ns syncretism.time-test
   (:require
    [clojure.test :refer :all]
-   [syncretism.time :refer :all])
+   [syncretism.time :refer :all]
+   [java-time :as jt])
   (:import
    [java.time ZoneId]
    [java.sql Timestamp]))
 
-(def offset
-  (-
-   (-> 
-    (Timestamp. 0)
-    (.toLocalDateTime)
-    (.atZone (ZoneId/systemDefault))
-    java-time/as-map
-    :offset-seconds)
+(def local-offset
+  (-> (System/currentTimeMillis)
+      (Timestamp.)
+      .toLocalDateTime
+      (.atZone (ZoneId/systemDefault))
+      jt/as-map
+      :offset-seconds
+      -))
 
-   (-> 
-    (Timestamp. 0)
-    (.toLocalDateTime)
-    (.atZone (ZoneId/of "Europe/Prague"))
-    java-time/as-map
-    :offset-seconds)))
+(defn to-timestamp
+  [d]
+  (let [{:keys [instant-seconds offset-seconds]}
+        (-> d
+            (java.time.OffsetDateTime/parse)
+            jt/as-map)]
+    (* 1000 (+ instant-seconds offset-seconds local-offset))))
 
 (deftest test-market-time
   (testing "market-time should report correct market period"
-    (let [tz (ZoneId/of "Europe/Prague")
-          ts-closed (+ 1625103790000 offset)
-          ts-pre (+ 1625118190000 offset)
-          ts-market (+ 1625136190000 offset)
-          ts-post (+ 1625161390000 offset)]
+    (let [tz (ZoneId/of "America/New_York")
+          ts-closed (to-timestamp "2021-07-02T01:00:00-04:00")
+          ts-pre (to-timestamp "2021-07-02T06:00:00-04:00")
+          ts-market (to-timestamp "2021-07-02T12:00:00-04:00")
+          ts-post (to-timestamp "2021-07-02T18:00:00-04:00")]
       (is (= (market-time tz ts-closed) "CLOSED"))
       (is (= (market-time tz ts-pre) "PRE"))
       (is (= (market-time tz ts-market) "OPEN"))

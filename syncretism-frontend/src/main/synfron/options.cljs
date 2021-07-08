b7ecba3367c53cf3c5da06750b9321a72d2d9ea1
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-07-09 01:01:47 +0800

    Added media query

--- syncretism-frontend/src/main/synfron/options.cljs
+++ syncretism-frontend/src/main/synfron/options.cljs
@@ -1,6 +1,33 @@
 (ns synfron.options
   (:require [synfron.state :as state]))
 
+(def columns-w-names
+  [[:contractSymbol "Contract Symbol" "CS"]
+   [:symbol "Symbol" "S"]
+   [:optType "Type" "T"]
+   [:strike "Strike" "Str"]
+   [:expiration "Expiration" "Exp"]
+   [:lastTradeDate "Last Trade Date" "LTD"]
+   [:impliedVolatility "Implied Volatility" "IV"]
+   [:bid "Bid" "B"]
+   [:ask "Ask" "A"]
+   [:lastPrice "Last Price" "LP"]
+   [:volume "Volume" "V"]
+   [:openInterest "Open Interest" "OI"]
+   [:yield "Yield" "Y"]
+   [:monthlyyield "Monthly Yield" "MY"]
+   [:inthemoney "In the Money" "ItM"]
+   [:pchange "Price Change" "PC"]
+   [:regularMarketPrice "Market Price" "MP"]
+   [:regularMarketDayLow "Market Day Low" "MDL"]
+   [:regularMarketDayHigh "Market Day High" "MDH"]
+   [:delta "Delta" "δ"]
+   [:gamma "Gamma" "γ"]
+   [:theta "Theta" "θ"]
+   [:vega "Vega" "ν"]
+   [:quotetype "Quote Type" "QT"]
+   [:lastCrawl "Last Updated" "LU"]])
+
 (defn render
   []
   [:div

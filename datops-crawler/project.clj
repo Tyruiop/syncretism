3f52a75e5aa8e0192d7bc6d9adb01dac03389a6d
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-07-16 11:03:16 +0800

    [crawler] Bump syncretism version

--- datops-crawler/project.clj
+++ datops-crawler/project.clj
@@ -13,7 +13,7 @@
                  [com.taoensso/timbre "5.1.2"]
                  [com.velisco/clj-ftp "0.3.12"]
 
-                 [org.clojars.tyruiop/syncretism "0.1.0"]]
+                 [org.clojars.tyruiop/syncretism "0.1.1"]]
   :main ^:skip-aot datops.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

b34306efe0983746c063c13ce94392f6865083db
Tyruiop <archibald.pontier+git@tyruiop.org>
2021-07-31 15:44:03 +0200

    [compute] Computing averages of multiple time series columns

--- datops-compute/src/datops_compute/db.clj
+++ datops-compute/src/datops_compute/db.clj
@@ -34,6 +34,70 @@
     (when (not-empty options)
       (recur limit {:last-seen (-> options last :contractsymbol)}))))
 
+(defn get-averages
+  [nb-days]
+  (let [t (int (/ (System/currentTimeMillis) 1000))
+        min-t (- t (* nb-days 3600 24))]
+    (with-open [con (jdbc/get-connection db)]
+      (jdbc/execute!
+       con
+       ["SELECT timeseries.contractSymbol,
+AVG(timeseries.volume) AS volume,
+AVG(timeseries.openinterest) AS oi,
+AVG(timeseries.impliedvolatility) AS iv,
+AVG(timeseries.premium) AS pre,
+AVG(timeseries.bid) AS bid,
+AVG(timeseries.delta) AS delta,
+AVG(timeseries.gamma) AS gamma,
+AVG(timeseries.theta) AS theta,
+AVG(timeseries.vega) AS vega,
+AVG(timeseries.rho) AS rho
+FROM timeseries LEFT JOIN live ON timeseries.contractsymbol = live.contractsymbol WHERE live.expiration > ? AND timeseries.timestamp > ? GROUP BY timeseries.contractSymbol"
+        t min-t]))))
+
+(defn float-or-nil [v] (try (float v) (catch Exception _ nil)))
+
+(defn flatten-vec
+  [v]
+  [(float-or-nil (get v :volume))
+   (float-or-nil (get v :oi))
+   (float-or-nil (get v :iv))
+   (float-or-nil (get v :pre))
+   (float-or-nil (get v :bid))
+   (float-or-nil (get v :delta))
+   (float-or-nil (get v :gamma))
+   (float-or-nil (get v :theta))
+   (float-or-nil (get v :vega))
+   (float-or-nil (get v :rho))
+   (get v :timeseries/contractSymbol)])
+
+(defn write-averages-20
+  []
+  (let [avgs (get-averages 20)
+        avgs-flat (map flatten-vec avgs)]
+    (println avgs-flat)
+    (with-open [con (jdbc/get-connection db)
+                ps
+                (jdbc/prepare
+                 con
+                 ["UPDATE live SET vol20d=?, oi20d=?, iv20d=?, pr20d=?,
+bid20d=?, delta20d=?, gamma20d=?, theta20d=?, vega20d=?, rho20d=?
+ WHERE contractsymbol=?"])]
+      (jdbp/execute-batch! ps avgs-flat))))
+
+(defn write-averages-100
+  []
+  (let [avgs (get-averages 100)
+        avgs-flat (map flatten-vec avgs)]
+    (with-open [con (jdbc/get-connection db)
+                ps
+                (jdbc/prepare
+                 con
+                 ["UPDATE live SET vol100d=?, oi100d=?, iv100d=?, pr100d=?,
+bid100d=?, delta100d=?, gamma100d=?, theta100d=?, vega100d=?, rho100d=?
+ WHERE contractsymbol=?"])]
+      (jdbp/execute-batch! ps avgs-flat))))
+
 (defn timeseries-table-def
   []
   (jdbc/execute!
